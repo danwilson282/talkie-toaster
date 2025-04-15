78e1bc4fbf1e401962a3c04565d8caaec2852c65
Dan Wilson <danwilson282@yahoo.co.uk>
2025-04-15 20:05:53 +0100

    added engine config for tts

--- pi_toaster.py
+++ pi_toaster.py
@@ -1,9 +1,10 @@
 import speech_recognition as sr
 import pyttsx3
 import requests
-
+engine = pyttsx3.init(driverName='espeak')
+engine.setProperty('rate', 200)  # Speed of speech (words per minute)
+engine.setProperty('volume', 1.0)  # Volume level (0.0 to 1.0)
 def speak(text):
-    engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
 
